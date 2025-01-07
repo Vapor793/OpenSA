173be8e2651bf94ee0b4866bff54a73f29a5741a
Vapor793 <754974990@qq.com>
2025-01-07 13:52:41 +0800

    none

--- OpenSA/WaveSelect/WaveSelcet.py
+++ OpenSA/WaveSelect/WaveSelcet.py
@@ -56,7 +56,7 @@ def SpctrumFeatureSelcet(method, X, y):
         X_Feature = X[:, Featuresecletidx]
     elif method == "Pca":
         X_Feature = Pca(X)
-        Featuresecletidx = = np.array([1, 1])
+        Featuresecletidx = np.array([1, 1])
     else:
         print("no this method of SpctrumFeatureSelcet!")
 
